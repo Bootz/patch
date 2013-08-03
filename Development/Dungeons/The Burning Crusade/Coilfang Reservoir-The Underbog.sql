cdc8a623c10b5d4d30b795865f54a9aec973c62a
cooler-SAI <cooler19860212@yahoo.com>
2013-08-03 18:10:39 +0400

    version fix

--- Development/Dungeons/The Burning Crusade/Coilfang Reservoir-The Underbog.sql
+++ Development/Dungeons/The Burning Crusade/Coilfang Reservoir-The Underbog.sql
@@ -1,4 +1,4 @@
-/* Copyright (C) 11 April 2011-2012 patch Project 
+/* Copyright (C) 11 April 2011-2013 patch Project 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
