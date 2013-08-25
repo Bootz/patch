a96c865e93aacc743ca824f35af390008cc11617
cooler-SAI <cooler19860212@yahoo.com>
2013-08-25 20:17:19 +0400

    - Wailing Weed fix

--- Development/Locations/Kalimdor/Mount Hyjal.sql
+++ Development/Locations/Kalimdor/Mount Hyjal.sql
@@ -1198,8 +1198,14 @@ SET @ENTRY := 40066;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,1,0,100,1,500,1000,0,0,11,74737,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Leafy Wail on Spawn'),
-(@ENTRY,0,1,0,0,0,100,0,3300,4500,11300,14500,11,80546,0,0,0,0,0,5,32,0,0,0,0,0,0,'Cast Bile Blast');
+(@ENTRY,0,0,0,4,0,100,1,0,0,0,0,22,1,0,0,0,0,0,1,0,0,0,0,0,0,0,'Set Phase 1 on Aggro'),
+(@ENTRY,0,1,0,4,1,100,1,0,0,0,0,11,48195,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Emerald Lasher Emerge on Aggro'),
+(@ENTRY,0,2,0,4,1,100,1,0,0,0,0,91,9,0,0,0,0,0,1,0,0,0,0,0,0,0,'Remove Ground Emote on Aggro'),
+(@ENTRY,0,3,0,0,1,100,0,4000,7000,15000,19000,11,74737,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Leafy Wail'),
+(@ENTRY,0,4,0,7,1,100,1,0,0,0,0,22,2,0,0,0,0,0,1,0,0,0,0,0,0,0,'Set Phase 2 on Evade'),
+(@ENTRY,0,5,0,7,2,100,1,0,0,0,0,91,9,0,0,0,0,0,1,0,0,0,0,0,0,0,'Remove Ground Emote on Evade'),
+(@ENTRY,0,6,0,21,2,100,1,0,0,0,0,90,9,0,0,0,0,0,1,0,0,0,0,0,0,0,'Set Ground Emote on Reached Home'),
+(@ENTRY,0,7,0,0,1,100,0,3300,4500,11300,14500,11,80546,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Bile Blast');
 
 -- Twilight Subjugator
 SET @ENTRY := 40463;
