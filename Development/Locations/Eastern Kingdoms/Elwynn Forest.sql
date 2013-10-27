af32e72b8769b9f4168dc87e9509755c7e3bf391
cooler-SAI <cooler19860212@yahoo.com>
2013-10-27 21:10:42 +0400

    added Blackrock Invader,Blackrock Spy,Goblin Assassin scripts, texts,emotes

--- Development/Locations/Eastern Kingdoms/Elwynn Forest.sql
+++ Development/Locations/Eastern Kingdoms/Elwynn Forest.sql
@@ -17,6 +17,38 @@
  * under certain conditions.
  */
 
+-- Blackrock Invader
+SET @ENTRY := 49874;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,4,0,70,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Aggro');
+-- NPC talk text insert
+SET @ENTRY := 49874;
+DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
+(@ENTRY,0,0, 'Orc KILL human!',12,0,50,0,0,0, 'on Aggro Text'),
+(@ENTRY,0,1, 'Blackrock take forest!',12,0,50,0,0,0, 'on Aggro Text'),
+(@ENTRY,0,2, 'The grapes were VERY TASTY!',12,0,50,0,0,0, 'on Aggro Text'),
+(@ENTRY,0,3, 'Eat you!',12,0,50,0,0,0, 'on Aggro Text'),
+(@ENTRY,0,4, 'Beg for life!',12,0,50,0,0,0, 'on Aggro Text');
+ 
+-- Blackrock Spy
+SET @ENTRY := 49874;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,4,0,70,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Aggro'),
+(@ENTRY,0,1,0,11,0,50,1,0,0,0,0,11,80676,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Spyglass on Spawn'),
+(@ENTRY,0,2,0,11,0,50,1,0,0,0,0,11,92857,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Spying on Spawn');
+-- NPC talk text insert
+SET @ENTRY := 49874;
+DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
+(@ENTRY,0,0, 'Orc KILL human!',12,0,50,0,0,0, 'on Aggro Text'),
+(@ENTRY,0,1, 'Blackrock take forest!',12,0,50,0,0,0, 'on Aggro Text'),
+(@ENTRY,0,2, 'The grapes were VERY TASTY!',12,0,50,0,0,0, 'on Aggro Text');
+ 
 -- Bodyguard
 SET @ENTRY := 6866;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
@@ -141,6 +173,20 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,0,1000,5000,12000,16000,11,13730,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Demoralizing Shout'),
 (@ENTRY,0,1,0,2,0,100,1,0,50,0,0,11,3238,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Nimble Reflexes at 50% HP');
+
+-- Goblin Assassin
+SET @ENTRY := 50039;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,4,0,60,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Aggro'),
+(@ENTRY,0,1,0,11,0,100,1,0,0,0,0,11,93046,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast So Sneaky on Spawn');
+-- NPC talk text insert
+SET @ENTRY := 50039;
+DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
+(@ENTRY,0,0, 'Time to join your friends, kissin\' the dirt!',12,0,50,0,0,0, 'on Aggro Text'),
+(@ENTRY,0,1, 'We\'re gonna burn this place to the ground!',12,0,50,0,0,0, 'on Aggro Text');
 
 -- Kobold Tunneler
 SET @ENTRY := 475;
