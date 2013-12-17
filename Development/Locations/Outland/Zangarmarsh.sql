23ac5727b2170190a0bab7243879f7703ed1e3e0
cooler-SAI <cooler19860212@yahoo.com>
2013-12-17 19:59:07 +0400

    * typo fixed Ango'rosh Sentry, thanks to dannyel

--- Development/Locations/Outland/Zangarmarsh.sql
+++ Development/Locations/Outland/Zangarmarsh.sql
@@ -86,7 +86,7 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,4,0,30,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Aggro'),
 (@ENTRY,0,1,0,0,0,100,0,2000,6000,4000,9000,11,15496,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Cleave'),
-(@ENTRY,0,1,0,0,0,100,0,5000,5000,10000,19000,11,13730,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Demoralizing Shout');
+(@ENTRY,0,2,0,0,0,100,0,5000,5000,10000,19000,11,13730,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Demoralizing Shout');
 -- NPC talk text insert
 SET @ENTRY := 20443;
 DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
