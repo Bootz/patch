0a8378a1f0874f623c9124f3fbc902dea963a2a2
cooler-SAI <cooler19860212@yahoo.com>
2013-12-22 20:31:24 +0400

    * removed unneeded SAI script, thanks to  NorthStrider

--- Development/Dungeons/Cataclysm/The Vortex Pinnacle.sql
+++ Development/Dungeons/Cataclysm/The Vortex Pinnacle.sql
@@ -138,12 +138,3 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,0,0,0,0,0,100,6,0,0,3400,4700,11,88032,64,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Lightning Bolt'),
 (@ENTRY,0,1,0,0,0,100,6,5000,7000,15000,18000,11,88010,0,0,0,0,0,4,0,0,0,0,0,0,0,'Cast Cyclone'),
 (@ENTRY,0,2,0,0,0,100,6,4000,9000,12000,14000,11,88029,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Wind Shock');
-
--- Young Storm Dragon
-SET @ENTRY := 45919;
-UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
-DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
-INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,0,0,100,6,5000,7000,12000,15000,11,88194,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Chilling Blast'),
-(@ENTRY,0,1,0,11,0,100,7,0,0,0,0,11,88192,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Brutal Strikes on Spawn'),
-(@ENTRY,0,2,0,2,0,100,7,0,40,0,0,11,88201,1,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Healing Well at 40% HP');
