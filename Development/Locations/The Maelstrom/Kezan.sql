fc57cddc1527128d64efe5af2e6bcac4f6fe5baa
cooler-SAI <cooler19860212@yahoo.com>
2013-12-31 19:02:49 +0400

    * fix Foreman Dampwick link

--- Development/Locations/The Maelstrom/Kezan.sql
+++ Development/Locations/The Maelstrom/Kezan.sql
@@ -226,8 +226,8 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,20,0,100,0,14138,0,0,0,1,0,3000,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Quest End'),
 (@ENTRY,0,1,2,52,0,100,0,0,34872,0,0,11,87226,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on SpellHit'),
-(@ENTRY,0,2,3,52,0,100,0,0,34872,0,0,1,1,3000,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on SpellHit'),
-(@ENTRY,0,3,0,52,0,100,0,1,0,0,0,1,2,3000,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on SpellHit'),
+(@ENTRY,0,2,3,61,0,100,0,0,0,0,0,1,1,3000,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on SpellHit'),
+(@ENTRY,0,3,0,61,0,100,0,0,0,0,0,1,2,3000,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on SpellHit'),
 (@ENTRY,0,4,0,19,0,100,0,25473,0,0,0,1,3,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Quest Add'),
 (@ENTRY,0,5,0,19,0,100,0,14124,0,0,0,1,4,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Quest Add'),
 (@ENTRY,0,6,0,1,0,100,0,45000,245000,45000,245000,1,5,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text OOC'),
