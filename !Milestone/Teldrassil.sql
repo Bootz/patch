670f074708a5e2576e786fa69df1fde1ad96a1b6
cooler-SAI <cooler19860212@yahoo.com>
2014-09-21 15:56:30 +0400

    * added Webwood Spider script

--- !Milestone/Teldrassil.sql
+++ !Milestone/Teldrassil.sql
@@ -393,6 +393,13 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,9,0,100,0,0,30,17000,20000,11,12023,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Web on Close');
 
+-- Webwood Spider
+SET @ENTRY := 1986;
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
+DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(@ENTRY,0,0,0,9,0,100,0,0,5,22000,24000,11,6751,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Weak Poison on Close');
+
 -- Vicious Grell
 SET @ENTRY := 2005;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
